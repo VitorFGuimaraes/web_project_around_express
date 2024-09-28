f26ab284f034ebba19161a4d687ebbd6aadac574
Vitor Guimarães <vitorrawwr@gmail.com>
2024-09-28 11:33:57 +0100

    users finalizado

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,27 @@
 import { Router } from "express";
-import  fs  from "fs";
-import  path  from "path";
-const router = Router();
-const dirname = path.resolve();
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
 
+const router = Router();
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 router.get("/", (req, res) => {
-  fs.readFile(path.join(dirname,'..', 'data','users.json'),  { encoding: "utf-8" }, (err,  data) => {
-    return res.json(data);
-  })
+  fs.readFile(path.join(__dirname, '..' , 'data', 'users.json'), "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Erro ao ler o arquivo" });
+    }
+
+    try {
+      const response = JSON.parse(data);
+      return res.json(response); //
+    } catch (parseError) {
+      return res.status(500).json({ error: "Erro ao parsear o JSON" });
+    }
+  });
 });
 
 export default router;
