f3f2d18dff34c9860ce2f42aec87dfa16d6d9f78
Vitor Guimarães <vitorrawwr@gmail.com>
2024-09-30 17:53:08 +0100

    Rota cards feita

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import usersRouter from "./routes/users.js";
+import cardRouter from "./routes/cards.js";
 
 const app = express();
 const port = 3000;
@@ -7,6 +8,8 @@ const port = 3000;
 app.use(express.json());
 
 app.use("/users", usersRouter);
+
+app.use("/cards", cardRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World")
---
+++ routes/cards.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const routerCard = Router();
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+routerCard.get("/", (req, res) => {
+  fs.readFile(path.join(__dirname, '..' , 'data', 'cards.json'), "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Erro ao ler o arquivo" });
+    }
+
+    try {
+      const response = JSON.parse(data);
+      return res.json(response);
+    } catch (parseError) {
+      return res.status(500).json({ error: "Erro ao parsear o JSON" });
+    }
+  });
+});
+
+export default routerCard;
